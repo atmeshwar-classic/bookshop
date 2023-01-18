adcf0fa53c6037c54c05211113afad591a373cb3
Atmeshwar <atmeshwar.singh@classicinformatics.com>
2023-01-18 13:03:12 +0530

    Book Card type changes

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -1,6 +1,12 @@
 import type { Book } from '../../domain/books/types'
 
-export const BookCard = (props: Book): JSX.Element => {
+/* 
+  We would define the structure of the props in the same file as the component.
+  Since, this component has the same props as Type Book, we would assign it to it props.
+*/
+type BookProps = Book;
+
+export const BookCard = (props: BookProps): JSX.Element => {
   return (
     <div>Book Card</div>
   )
