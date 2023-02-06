d349895e4436a0e69ce295673e2306209be84241
Saraswathi <saraswathi@classicinformatics.com>
2023-02-06 19:18:35 +0530

    updated react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import { BooksPage } from './domain/books/BooksPage'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { CartPage } from './domain/cart/CartPage'
-import Navbar from './components/Navbar/Navbar'
+
+import "./App.css";
+import { BooksPage } from "./domain/books/BooksPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { CartPage } from "./domain/cart/CartPage";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
 
   return (
-    <div className="App">
-      <div className="card">
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path='/' element={<BooksPage />} />
-            <Route path='/cart' element={<CartPage />} />
-            <Route />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<BooksPage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+    </Router>
   )
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,8 @@ import { store } from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
