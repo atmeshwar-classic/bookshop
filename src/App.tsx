f7c81ce17c445b6d146e27bb2057e297d949e4e6
Saraswathi <saraswathi@classicinformatics.com>
2023-02-06 11:53:44 +0530

    Added react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { BooksPage } from './domain/books/BooksPage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { CartPage } from './domain/cart/CartPage'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
     <div className="App">
       <div className="card">
-        <BooksPage />
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<BooksPage />} />
+            <Route path='/cart' element={<CartPage />} />
+            <Route />
+          </Routes>
+        </BrowserRouter>
       </div>
     </div>
   )
