a2fe42769c9c0244e1d8a34c9c169c436c683652
Saraswathi <saraswathi@classicinformatics.com>
2023-02-06 19:47:25 +0530

    Added book card and bookpage

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -1,5 +1,7 @@
-import type { Book } from '../../domain/books/types'
-
+import { useDispatch } from "react-redux";
+import type { Book } from "../../domain/books/types";
+import { addToCart } from "../../domain/cart/cartSlice";
+import type { AppDispatch } from "../../store/store";
 /* 
   We would define the structure of the props in the same file as the component.
   Since, this component has the same props as Type Book, we would assign it to it props.
@@ -7,13 +9,22 @@ import type { Book } from '../../domain/books/types'
 type BookProps = Book;
 
 export const BookCard = (props: BookProps): JSX.Element => {
-  const {id, name, author, description, price} = props;
+  const dispatch: AppDispatch = useDispatch();
+
   return (
-    <div className='book-card' key={id}>
-      <div>{name}</div>
-      <div>{author}</div>
-      <div>{description}</div>
-      <div>{price}</div>
+    <div className="book-card">
+      <div className="book-card-img">
+        <img alt="Cover Page" />
+      </div>
+      <div className="book-card-contents">
+        <h1>{props.name}</h1>
+        <h3>{props.author}</h3>
+        <p>{props.description}</p>
+        <p>${props.price}</p>
+      </div>
+      <div className="book-card-btn">
+        <button onClick={() => dispatch(addToCart(props))}>Add to Cart</button>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/domain/books/BooksPage.tsx
+++ src/domain/books/BooksPage.tsx
@@ -1,22 +1,24 @@
+import { useSelector } from "react-redux";
 import { BookCard } from "../../components/BookCard";
-import { books } from "./books";
+import { booksSelector } from "./booksSlice";
+import type { Book } from "./types";
 
 export const BooksPage = (): JSX.Element => {
+  const { books, loading } = useSelector(booksSelector);
+
   return (
     <div className="book-page">
-      {books.map((book) => {
-        const { id, name, author, description, price } = book;
-        console.log(name)
-        return (
-            <BookCard
-              id={id}
-              name={name}
-              author={author}
-              description={description}
-              price={price}
-            />
-        );
-      })}
+      {loading == "loading" ? (
+        "Loading ..."
+      ) : (
+        <div className="book-page-container">
+          <div className="book-page-container-flex">
+            {books.map((book:Book) => (
+              <BookCard key={book.id} {...book}/>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
