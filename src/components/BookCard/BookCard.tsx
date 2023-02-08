7001622260c64e086f51aa347e1a6cb0764b60ac
ansaf-1246 <ansaf.akhtar@classicinformatics.com>
2023-02-08 22:40:01 +0530

    Book page changes

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -1,30 +1,31 @@
-import type { Book } from '../../domain/books/types'
-import { books } from '../../domain/books/books';
-import "./styles.css"
-import { useDispatch } from 'react-redux';
-import { addBookToCart } from '../../domain/cart/cartSlice';
+import type { Book } from "../../domain/books/types";
+import "./styles.css";
+import { useDispatch } from "react-redux";
+import { addBookToCart } from "../../domain/cart/cartSlice";
 /* 
   We would define the structure of the props in the same file as the component.
   Since, this component has the same props as Type Book, we would assign it to it props.
 */
 type BookProps = Book;
-// interface BookProps {
-//   Book: Book;
-// }
 
-export const BookCard = ( book : BookProps): JSX.Element => {
-
+export const BookCard = (book: BookProps): JSX.Element => {
   const dispatch = useDispatch();
-  
+
   return (
-    <div className='card-container' key={book.id}>
-    <div className='book-img'>Book Image</div>
-    <h5>{book.name}</h5>
-    <h5>{book.author}</h5>
-    <p>{book.description}</p>
-    <p>{book.price}</p>
-    <button onClick={() => dispatch(addBookToCart(book))}
-    >Add to Cart</button>
+    <div className="card-container" key={book.id}>
+      <div className="book-img">
+        <h4>Book Image</h4>
+        <div
+          className="add-button"
+          onClick={() => dispatch(addBookToCart(book))}
+        >
+          <strong>+</strong>
+        </div>
+      </div>
+      <h5>Title: {book.name}</h5>
+      <h5>Author: {book.author}</h5>
+      <h6>Description: {book.description}</h6>
+      <p>$ {book.price}</p>
     </div>
-  )
-}
+  );
+};
--- src/domain/books/BooksPage.tsx
+++ src/domain/books/BooksPage.tsx
@@ -1,31 +1,29 @@
 import { useDispatch, useSelector } from "react-redux";
 import { BookCard } from "../../components/BookCard";
-import { books } from "./books";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./styles.css";
 import { AppDispatch } from "../../store/store";
 import { booksSelector, getBooks } from "./booksSlice";
-import { Book } from "./types";
 
 export const BooksPage = (): JSX.Element => {
   const dispatch: AppDispatch = useDispatch();
   const Bookdata = useSelector(booksSelector);
 
-  const [booksData, setBooksData] = useState<Book[]>(Bookdata);
-
   useEffect(() => {
     dispatch(getBooks());
-  }, []);
-  useEffect(() => {
-    setBooksData(Bookdata);
   }, [Bookdata]);
 
   return (
     <div className="book-page-container">
-      {booksData.length === 0 ? (
+      <>{console.log(Bookdata)}</>
+      {Bookdata.length === 0 ? (
         <div>Loading...</div>
       ) : (
-        booksData.map((book) => <BookCard key={book.id} {...book} />)
+        <div className="book-list">
+          {Bookdata.map((book) => (
+            <BookCard {...book} key={book.id} />
+          ))}
+        </div>
       )}
     </div>
   );
