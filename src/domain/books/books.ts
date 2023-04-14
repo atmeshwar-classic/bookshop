68995ff55f15307f01b3f873f26e7f25a12584e3
osrao79 <onkar.rao@classicinformatics.com>
2023-04-14 08:12:47 +0530

    created UI for Bookpage and Book Card

--- src/domain/books/books.ts
+++ src/domain/books/books.ts
@@ -5,42 +5,42 @@ export const books: Book[] = [
     id: '123',
     name: 'Book',
     author: 'Me',
-    description: 'A very good book',
+    description: 'Neque porro quisquam est qui dolorem...',
     price: 20
   },
   {
     id: '124',
     name: 'Book',
     author: 'Me',
-    description: 'A very good book',
+    description: 'Neque porro quisquam est qui dolorem...',
     price: 20
   },
   {
     id: '125',
     name: 'Book',
     author: 'Me',
-    description: 'A very good book',
+    description: 'Neque porro quisquam est qui dolorem...',
     price: 20
   },
   {
     id: '123',
     name: 'Book',
     author: 'Me',
-    description: 'A very good book',
+    description: 'Neque porro quisquam est qui dolorem...',
     price: 20
   },
   {
     id: '126',
     name: 'Book',
     author: 'Me',
-    description: 'A very good book',
+    description: 'Neque porro quisquam est qui dolorem...',
     price: 20
   },
   {
     id: '127',
     name: 'Book',
     author: 'Me',
-    description: 'A very good book',
+    description: 'Neque porro quisquam est qui dolorem...',
     price: 20
   }
 ]
