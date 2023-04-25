efa24becf4fd9a33762a444e653a69e759827308
Aftab Alam <aftab.alam@classicinformatics.com>
2023-04-25 11:54:11 +0530

    Add nevigation of books and cart

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -1,13 +1,44 @@
-import type { Book } from '../../domain/books/types'
+import { useState } from "react";
+import { BooksPage } from "../../domain/books/BooksPage";
+import type { Book } from "../../domain/books/types";
+import { CartPage } from "../../domain/cart/CartPage";
+import { HashRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 /* 
   We would define the structure of the props in the same file as the component.
   Since, this component has the same props as Type Book, we would assign it to it props.
 */
-type BookProps = Book;
+// interface BookProps {
+//   books: Book[];
+// }
 
-export const BookCard = (props: BookProps): JSX.Element => {
+export const BookCard = (): JSX.Element => {
+  const [bookData, setBookData] = useState<Book[]>([]);
+
+  const handleClick = (e: React.MouseEvent, book: Book) => {
+    // console.log("hello there!!!:", book);
+    setBookData([...bookData, book]);
+  };
+
   return (
-    <div>Book Card</div>
-  )
-}
+    <Router>
+      <header className="header">
+        <nav>
+          <Link to="/books">
+            <button  className="btn">Books</button>
+          </Link>
+          <Link to="/cart">
+            <button className="btn">Cart</button>
+          </Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route
+          path="/books"
+          element={<BooksPage handleClick={handleClick} />}
+        />
+        <Route path="/cart" element={<CartPage books={bookData} />} />
+      </Routes>
+    </Router>
+  );
+};
